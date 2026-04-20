4c7bd45661f60f719a8c36264a79d09eb6840963
meet-17804 <pma2819@gmail.com>
2026-04-20 11:13:04 +0530

    Initial commit

--- middleware/authmiddleware.js
+++ middleware/authmiddleware.js
@@ -1,19 +1,23 @@
 const jwt = require("jsonwebtoken");
 
-const protect = (req,res,next) =>{
-    const token = req.headers.authorization;
+const protect = (req, res, next) => {
+  const authHeader = req.headers.authorization;
 
-    if(!token){
-        res.status(401).json({message: "No token provided"});
-    }
+  if (!authHeader) {
+    return res.status(401).json({ message: "No token provided" });
+  }
 
-    try{
-        const decode = jwt.varify(token, process.env.JWT_SECRET)
-        req.userCreated = decode.id;
-        next();
-    } catch(error){
-        res.status(401).json({message: "Invalid token"});
-    }
+  try {
+    const token = authHeader.split(" ")[1];
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    req.userId = decoded.id;
+
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: "Invalid token" });
+  }
 };
 
 module.exports = protect;
