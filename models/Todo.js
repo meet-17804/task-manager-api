6c7c70e3677bf62dc0c126210db28ac44ba40555
meet-17804 <pma2819@gmail.com>
2026-05-14 16:10:28 +0530

    Initial commit

--- models/Todo.js
+++ models/Todo.js
@@ -24,6 +24,20 @@ const todoSchema = new mongoose.Schema(
     default: "Medium",
    },
 
+   status: {
+
+   type: String,
+
+   enum: [
+    "Pending",
+    "In Progress",
+    "Completed"
+   ],
+
+   default: "Pending",
+
+   },
+
     user: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
--- validators/todoValidator.js
+++ validators/todoValidator.js
@@ -4,7 +4,8 @@ const todoSchema = z.object({
     task: z.string().min(1, "Task is required"),
     dueDate: z.string().transform((val) => new Date(val)),
     completed: z.boolean().optional(),
-    priority: z.enum(["Low","Medium","High",]).optional(),
+    priority: z.enum(["Low","Medium","High"]).optional(),
+    status: z.enum(["Pending","In Progress","Completed"]).optional(),
 });
 
 module.exports = {todoSchema};
